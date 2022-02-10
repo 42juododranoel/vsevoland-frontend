bf18feb023730398e3a5040d62c1568557684702
Vsevolod Skripnik <vsevolod_skripnik@protonmail.com>
2022-02-10 22:14:27 +0300

    Add category images

--- components/blog/PostList.vue
+++ components/blog/PostList.vue
@@ -1,43 +1,39 @@
 <template>
   <div class="post-list">
-    <div v-if="category" class="post-list--header">
-      <div class="row">
-        <div class="col-10-md">
-          <Heading1>{{ category.title }}</Heading1>
-          <Paragraph class="post-list--description">
-            {{ category.description }}
-          </Paragraph>
-          <div class="post-list--labels">
-            <span class="post-list--labels-prefix">Рубрики:</span>
-            <span class="post-list--labels-content">
-              <Link
-                v-for="(label, labelIndex) in labels"
-                :key="labelIndex"
-                class="post-list--label"
-                prefix="#"
-                :postfix="labelIndex + 1 !== labels.length ? ',' : undefined"
-                :target="`/${category.slug.current}/labels/${label.slug.current}`"
-              >
-                {{ label.title }}
-              </Link>
-            </span>
+    <div class="row">
+      <div class="col-7-md">
+        <Heading1>{{ category.title }}</Heading1>
+        <div class="post-list--labels">
+          <span class="post-list--labels-prefix">Рубрики:</span>
+          <span class="post-list--labels-content">
+            <Link
+              v-for="(label, labelIndex) in labels"
+              :key="labelIndex"
+              class="post-list--label"
+              prefix="#"
+              :postfix="labelIndex + 1 !== labels.length ? ',' : undefined"
+              :target="`/${category.slug.current}/labels/${label.slug.current}`"
+            >
+              {{ label.title }}
+            </Link>
+          </span>
+        </div>
+        <div class="post-list--content">
+          <div v-for="(post, postIndex) in posts" :key="postIndex">
+            <PostListItem
+              :title="post.title"
+              :description="post.description"
+              :slug="post.slug"
+              :labels="post.labels"
+              :category="post.category"
+              :do-show-category="!category"
+              :published-at="post.publishedAt"
+            />
           </div>
         </div>
       </div>
-    </div>
-    <div class="post-list--content">
-      <div v-for="(post, postIndex) in posts" :key="postIndex" class="row">
-        <div class="col-7-md">
-          <PostListItem
-            :title="post.title"
-            :description="post.description"
-            :slug="post.slug"
-            :labels="post.labels"
-            :category="post.category"
-            :do-show-category="!category"
-            :published-at="post.publishedAt"
-          />
-        </div>
+      <div v-if="category.background" class="off-8-md col-8-md">
+        <Picture :asset="category.background.asset" />
       </div>
     </div>
   </div>
@@ -46,12 +42,12 @@
 <script>
 import PostListItem from '~/components/blog/PostListItem.vue'
 import Heading1 from '~/components/typography/Heading1.vue'
-import Paragraph from '~/components/typography/Paragraph.vue'
 import Link from '~/components/typography/Link.vue'
+import Picture from '~/components/typography/Picture.vue'
 
 export default {
   name: 'PostList',
-  components: { PostListItem, Heading1, Paragraph, Link },
+  components: { PostListItem, Heading1, Link, Picture },
   props: {
     posts: {
       type: Array,
@@ -85,6 +81,13 @@ export default {
 
   &--labels {
     font-family: $font-family-sans;
+    margin-bottom: 63px;
+
+    &::after {
+      display: block;
+      content: '';
+      clear: both;
+    }
   }
 
   &--labels-prefix {
